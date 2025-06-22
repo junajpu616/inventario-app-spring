5128a687b93c89e4665f909b4726b446780881fb
Junajpu <113324586+junajpu616@users.noreply.github.com>
2025-06-21 22:30:21 -0600

    Se agrego método de Actualizar y Eliminar
    Se agrego método de Actualizar y Eliminar, se eliminó lombok y se realizó todo manualmente y se creo un Exception

--- src/main/java/cs/inventarios/modelo/Producto.java
+++ src/main/java/cs/inventarios/modelo/Producto.java
@@ -1,14 +1,10 @@
 package cs.inventarios.modelo;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +12,68 @@ public class Producto {
     String descripcion;
     Double precio;
     Integer existencia;
+
+    public Producto() {
+    }
+
+    public Producto(Integer idProducto, String descripcion, Double precio, Integer existencia) {
+        this.idProducto = idProducto;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.existencia = existencia;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(Integer existencia) {
+        this.existencia = existencia;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return Objects.equals(idProducto, producto.idProducto) && Objects.equals(descripcion, producto.descripcion) && Objects.equals(precio, producto.precio) && Objects.equals(existencia, producto.existencia);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idProducto, descripcion, precio, existencia);
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "idProducto=" + idProducto +
+                ", descripcion='" + descripcion + '\'' +
+                ", precio=" + precio +
+                ", existencia=" + existencia +
+                '}';
+    }
 }
